9992e2e341008f2e9e18335243371ede23872b05
NourheneB <benrhoumanourhene@gmail.com>
2019-02-07 17:43:39 +0100

    Drop MIGRATION

--- src/ContosoUniversity/Migrations/201902071618095_derniereBrancheMaster.cs
+++ src/ContosoUniversity/Migrations/201902071618095_derniereBrancheMaster.cs
@@ -3,16 +3,14 @@ namespace ContosoUniversity.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class ModificationControlleur : DbMigration
+    public partial class derniereBrancheMaster : DbMigration
     {
         public override void Up()
         {
-            DropColumn("dbo.Person", "Role");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Person", "Role", c => c.String());
         }
     }
 }
