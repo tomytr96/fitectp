7668543e4d5510c0cc52044d5b515b912ebc0f66
NourheneB <benrhoumanourhene@gmail.com>
2019-02-07 14:47:27 +0100

    Suppression UserAccount de Migration

---
+++ src/ContosoUniversity/Migrations/201902071344213_AjoutdeAPIEtudient.cs
@@ -0,0 +1,18 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AjoutdeAPIEtudient : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
