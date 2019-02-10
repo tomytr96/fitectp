55c30118b8187653651a7c33913419490d328303
Tomy Trimoreau <tomy.trimoreau@hotmail.com>
2019-02-10 21:48:39 +0100

    Ajout des méthodes de tests dans le StudentController

--- tests/ContosoUniversity.Tests/Controllers/StudentControllerTests.cs
+++ tests/ContosoUniversity.Tests/Controllers/StudentControllerTests.cs
@@ -1,92 +1,109 @@
-﻿//using ContosoUniversity.Controllers;
-//using ContosoUniversity.DAL;
-//using ContosoUniversity.Models;
-//using ContosoUniversity.Tests.Tools;
-//using ContosoUniversity.ViewModels;
-//using Moq;
-//using NUnit.Framework;
-//using System.Web;
-//using System.Web.Mvc;
-//using System.Web.Routing;
+﻿using ContosoUniversity.Controllers;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.Tests.Tools;
+using ContosoUniversity.ViewModels;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 
-//namespace ContosoUniversity.Tests.Controllers
-//{
-//    public class StudentControllerTests : IntegrationTestsBase
-//    {
-//        private MockHttpContextWrapper httpContext;
-//        private StudentController controllerToTest;
-//        private SchoolContext dbContext;
-//        private StudentVM vm = new StudentVM();
+namespace ContosoUniversity.Tests.Controllers
+{
+    public class StudentControllerTests : IntegrationTestsBase
+    {
+        private MockHttpContextWrapper httpContext;
+        private StudentController controllerToTest;
+        private SchoolContext dbContext;
 
-//        [SetUp]
-//        public void Initialize()
-//        {
-//            httpContext = new MockHttpContextWrapper();
-//            controllerToTest = new StudentController();
-//            controllerToTest.ControllerContext = new ControllerContext(httpContext.Context.Object, new RouteData(), controllerToTest);
-//            dbContext = new DAL.SchoolContext(this.ConnectionString);
-//            controllerToTest.DbContext = dbContext;
-//        }
+        private readonly StudentVM validStudent = new StudentVM()
+        {
+            ID = 20,
+        };
+        private readonly StudentVM invalidStudent = new StudentVM()
+        {
+            ID = 999999,
+        };
 
-//        [Test]
-//        public void GetDetails_ValidStudent_Success()
-//        {
-//            string expectedLastName = "Dubois";
-//            string expectedFirstName = "George";
+        [SetUp]
+        public void Initialize()
+        {
+            httpContext = new MockHttpContextWrapper();
+            controllerToTest = new StudentController();
+            controllerToTest.ControllerContext = new ControllerContext(httpContext.Context.Object, new RouteData(), controllerToTest);
+            dbContext = new DAL.SchoolContext(this.ConnectionString);
+            controllerToTest.DbContext = dbContext;
+        }
 
-//            EntityGenerator generator = new EntityGenerator(dbContext);
-//            Student student = generator.CreateStudent(expectedLastName, expectedFirstName);
+        //[Test]
+        //public void GetDetails_InvalidStudent_Success()
+        //{
+        //    string expectedLastName = "Dubois";
+        //    string expectedFirstName = "George";
 
-//            var result = controllerToTest.Details(vm) as ViewResult;
+        //    EntityGenerator generator = new EntityGenerator(dbContext);
+        //    Student student = generator.CreateStudent(expectedLastName, expectedFirstName);
 
-//            var resultModel = result.Model as Student;
+        //    var result = controllerToTest.Details(invalidStudent) as ViewResult;
 
-//            Assert.That(result, Is.Not.Null);
-//            Assert.That(resultModel, Is.Not.Null);
-//            Assert.That(expectedLastName, Is.EqualTo(resultModel.LastName));
-//            Assert.That(expectedFirstName, Is.EqualTo(resultModel.FirstMidName));
-//        }
+        //    var resultModel = result.Model as Student;
 
-//        [Test]
-//        public void GetDetails_InvalidStudent_Fail404()
-//        {
-//            const int expectedStatusCode = 404;
-//            const int invalidId = 99999999;
-//            //SECOND PARAMETRE A REMPLACER
-//            var result = controllerToTest.Details(vm) as HttpStatusCodeResult;
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That(resultModel, Is.Not.Null);
+        //    Assert.That(expectedLastName, Is.EqualTo(resultModel.LastName));
+        //    Assert.That(expectedFirstName, Is.EqualTo(resultModel.FirstMidName));
+        //}
 
-//            Assert.That(result, Is.Not.Null);
-//            Assert.That(expectedStatusCode, Is.EqualTo(result.StatusCode));
-//        }
+        //[Test]
+        //public void GetDetails_ValidStudent_DetailsStudent()
+        //{
+        //    string expectedFirstName = "Tomy";
+        //    string expectedLastName = "TRIMOREAU";
 
-//        [Test]
-//        public void Edit_ValidStudentData_Success()
-//        {
-//            string expectedLastName = "Wood";
-//            string previousLastName = "Dubois";
-//            string previousFirstName = "George";
+        //    DateTime expectedEnrollmentDate = new DateTime(05,9,1);
+        //    var result = controllerToTest.Details(validStudent) as ViewResult;
+
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That((result.Model as StudentVM).FirstMidName, Is.EqualTo(expectedFirstName));
+        //    Assert.That((result.Model as StudentVM).FirstMidName, Is.EqualTo(expectedLastName));
+        //    Assert.That((result.Model as StudentVM).EnrollmentDate, Is.EqualTo(expectedEnrollmentDate));
+        //}
+
+        //[Test]
+        //public void GetDetails_InvalidStudent_Fail404()
+        //{
+        //    const int expectedStatusCode = 404;
+        //    var result = controllerToTest.Details(invalidStudent) as HttpStatusCodeResult;
+
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That(expectedStatusCode, Is.Not.Null);
+        //    Assert.That(expectedStatusCode, Is.EqualTo(result.StatusCode));
+        //}
+
+        //[Test]
+        //public void Edit_ValidStudentData_Success()
+        //{
+        //    string expectedLastName = "Wood";
+        //    string previousLastName = "Dubois";
+        //    string previousFirstName = "George";
 
 
-//            EntityGenerator generator = new EntityGenerator(dbContext);
-//            Student student = generator.CreateStudent(previousLastName, previousFirstName);
-//            student.LastName = expectedLastName;
+        //    EntityGenerator generator = new EntityGenerator(dbContext);
+        //    Student student = generator.CreateStudent(previousLastName, previousFirstName);
+        //    student.LastName = expectedLastName;
 
-//            FormDataHelper.PopulateFormData(controllerToTest, student);
+        //    FormDataHelper.PopulateFormData(controllerToTest, student);
 
-//            var result = controllerToTest.EditPost(student.ID) as ViewResult;
+        //    var result = controllerToTest.EditPost(student.ID) as ViewResult;
 
-//            Student savedStudent = dbContext.Students.Find(student.ID);
+        //    Student savedStudent = dbContext.Students.Find(student.ID);
 
-//            Assert.That(result, Is.Not.Null);
-//            Assert.That((result.Model as Student).LastName, Is.EqualTo(expectedLastName));
-//            Assert.That(savedStudent.LastName, Is.EqualTo(expectedLastName));
-//        }
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That((result.Model as Student).LastName, Is.EqualTo(expectedLastName));
+        //    Assert.That(savedStudent.LastName, Is.EqualTo(expectedLastName));
+        //}
 
-
-//        //[Test]
-//        //public void AccountWithBlankPassword()
-//        //{
-//        //    Assert.False(true);
-//        //}
-//    }
-//}
+    }
+}
